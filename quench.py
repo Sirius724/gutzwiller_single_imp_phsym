e63146461da43722097535f3f83b98cac1b389ac
Pier Paolo Baruselli <barusell@venus.cm.sissa.it>
2016-10-05 13:41:46 +0200

    fixed time evolution

--- quench.py
+++ quench.py
@@ -22,32 +22,23 @@ def f2(x,e,pend):
 	g=f(x,e)-pend*x
 	return g
 
-
-#def epsilon_n(n,d,delta):return delta*(n+d)
-#epsilon_n_v= np.vectorize(epsilon_n)
-
-#def alfa_m(m,d,A,N): return 1/pi*(atan(A*N/(m+d+1e-8)).real)
-#alfa_m_v= np.vectorize(alfa_m)
-
-#def E_m(m,d,delta,A,N): return epsilon_n(m,d,delta)+delta*alfa_m(m,d,A,N)
-#E_m_v= np.vectorize(E_m)
-
-#def g_m(m,d,delta,A,N,V): return np.sqrt(1/(1+pi**2*V**2*A**2*N/(2*(A**2*delta**2*N**2+epsilon_n(m,d,delta)**2))))
-#def g_m(m,d,delta,A,N,V): return np.sqrt(1/(1+pi**2*V**2/(2*N*delta**2*np.sin(pi*(alfa_m(m,d,A,N)))**2)))
-#g_m_v= np.vectorize(g_m)
-
-#def lambda_mn(m,n,d,delta,A,N,V): return g_m(m,d,delta,A,N,V)*V/(E_m(m,d,delta,A,N)-epsilon_n(n,d,delta))
-#lambda_mn_v= np.vectorize(lambda_mn)
+def occup(E,mu,T):
+#	return 1./(np.exp((E-mu)/T)+1)
+	if(E-mu >=0):	return np.exp(-(E-mu)/T)/(np.exp(-(E-mu)/T)+1)
+	if(E-mu <0):	return 1/(np.exp((E-mu)/T)+1)
+occup_v=np.vectorize(occup)
 
 def main():
 	Delta=1.
-	N=50
+	N=200
 	delta=Delta/N
 	d=0.5
 	V=1
 	A=pi*V**2/(2*Delta**2) #/(2*N)
-	tau=0.4578
-
+	tau=10
+	mu=0
+	T=0.001
+	n_step=1000
 
 	epsilon_v=np.zeros((2*N))
 	alfa_v=np.zeros((2*N))		#energy o phase shift for cond. electrons
@@ -57,34 +48,34 @@ def main():
 	m_v=range(-N,N+1)
 	n_v=range(-N,N)
 
-#	print(lambda_v)
-
-#	epsilon_v=epsilon_n_v(n_v,d,delta)
-#	alfa_v=alfa_m_v(m_v,d,A,N)
-#	E_v=E_m_v(m_v,d,delta,A,N)
-#	g_v=g_m_v(m_v,d,delta,A,N,V)
-	#lambda_v=lambda_mn_v(m_v,n_v,d,delta,A,N,V)
 
 
-#	for m in range(-N,N):
-#		g_v[m+N]=g_m(m,d,delta,A,N,V)
-#		epsilon_v[m+N]=epsilon_n(m,d,delta)
-#		alfa_v[m+N]=alfa_m(m,d,A,N)
-#		E_v[m+N]=E_m(m,d,delta,A,N)
-#		for n in range(-N,N):
-#			lambda_v[m+N,n+N]=lambda_mn(m,n,d,delta,A,N,V)
-#			lambda_v[m+N,n+N]=
-
+#Energies
 	for n in range(-N,N):
 		epsilon_v[n+N]=delta*(n+d)
 		alfa_v[n+N]=1/pi*(atan(A*N/(n+d+1e-8)).real)
-#		g_v[m+N]=np.sqrt(1/(1+pi**2*V**2/(2*N*delta**2*N**2*A**2/(N**2*A**2+(m+d+alfa_v[m+N])**2))))	#exact
-#		g_v[m+N]=np.sqrt(1/(1+pi**2*V**2/(2*N*delta**2*N**2*A**2/(N**2*A**2+(m+d)**2))))		#approx
 	for m in range(-N,N):
 		E_v[m+N]=epsilon_v[m+N]+delta*alfa_v[m+N]
 	E_v[2*N]=0		#bound state at zero energy
 
-#computes energies numerically with fsolve
+
+#Hamiltonian
+	Ham=np.zeros((2*N+1,2*N+1))	#last state is d
+	for n in range(-N,N):
+		Ham[n+N,n+N]=epsilon_v[n+N]
+		Ham[2*N,n+N]=V/np.sqrt(2*N)
+		Ham[n+N,2*N]=V/np.sqrt(2*N)
+
+
+#diagonalizes Hamiltonian
+
+	energies,rot_mat0=np.linalg.eig(Ham)
+
+	#	print(energies)
+
+
+
+#computes energies numerically with fsolve satrting with approx solution E_v
 	E_true=np.zeros(2*N+1)
 	for i,ei in enumerate(E_v):
 		E_true[i]=spo.fsolve(f2,E_v[i],args=(epsilon_v,2*N*Delta/V**2))
@@ -92,7 +83,7 @@ def main():
 #	alfa_true=(E_true-epsilon_v)/delta
 
 
-#	print (E_true)
+	#	print (E_true)
 
 	for m in range(-N,N+1):
 		sum_n=0.
@@ -109,14 +100,27 @@ def main():
 
 
 
-#	print(np.transpose(np.asmatrix(g_v)).shape)
+# Creates matrix of eigenvectors
 	rot_mat=np.concatenate((lambda_v/np.sqrt(2.*N),np.transpose(np.asmatrix(g_v))),axis=1)
+
+
+#Checks that it diagonalizes Hamiltonian
+	Ham_diag=np.dot(rot_mat,np.dot(Ham,np.transpose(rot_mat)))
+	Ham_diag2=np.dot(np.transpose(rot_mat0),np.dot(Ham,rot_mat0))
+#	plt.matshow(Ham)
+#	plt.matshow(Ham_diag)
+#	plt.matshow(Ham_diag2)
+#	plt.show()
+
+
+#	print(rot_mat-np.transpose(rot_mat0))
 
 
 #	print(lambda_v.shape)
 #	print(rot_mat.shape)
 
 	print("det=",np.linalg.det(rot_mat))
+	print("det0=",np.linalg.det(rot_mat0))
 #	print(lambda_v)
 
 #	plt.plot(m_v,epsilon_v,'r',label='epsilon')
@@ -180,21 +184,28 @@ def main():
 	y=V**2/(2*N*Delta)*f(x,epsilon_v)
 
 
-#Plotting
+#Plotting graphic method
 
-	if (n<30):
+	if (N<30):
 
 		plt.plot(x,y,'b',label='g')
 		plt.plot(x,x,'r',label='g')
-		plt.vlines(epsilon_v,-1000,1000,linewidth=4)
+		plt.vlines(epsilon_v,-1000,1000,linewidth=3)
 		plt.vlines(E_v,-1000,1000,linewidth=3,linestyles='dashed',colors='g')
 		plt.vlines(E_true,-1000,1000,linewidth=2,colors='y')
+		plt.vlines(energies,-1000,1000,linewidth=2,linestyles='dotted',colors='c')
 		plt.ylim(-1.4,1.4)
 		plt.xlim(-1.4,1.4)
-#	plt.legend()
+	#	plt.legend()
 		plt.show()
 
 
+
+
+#Checks that the two methods are consistent
+#	print(np.sort(energies))
+#	print(np.sort(E_true))
+	assert(np.all(abs(np.sort(energies)-np.sort(E_true))<1e-8))
 
 	epsilon_vm=np.zeros((2*N+1))
 	for n in range(-N,N):
@@ -202,98 +213,78 @@ def main():
 	epsilon_vm[2*N]=0			# I add the d state at zero energy
 
 
-###########Energies at the third interval H0 H1 H0
-
-	U_mm=np.zeros((2*N+1,2*N+1),dtype=complex)
-	for m in range(-N,N+1):
-		U_mm[m+N,m+N]=np.exp(-1j*E_v[n+N]*tau)
-
-
-	U_npn=np.dot(np.transpose(rot_mat),np.dot(U_mm,rot_mat))
-
-#	print(U_mm)
-
-
-
-	E_new2=0.
-	for m in range(-N,N+1):
-		for n in range(-N,0):
-			E_new2+=np.absolute(U_npn[m+N,n+N])**2*epsilon_vm[m+N]
-	E_new2=E_new2/N
-
-
-	print(E_old,E_new,E_new2)	#H_0,H_1,H_0
-
-
-###########Energies at the third interval H1 H0 H1 
-
-	U_mm2=np.zeros((2*N+1,2*N+1),dtype=complex)
-	for m in range(-N,N+1):
-		U_mm2[m+N,m+N]=np.exp(-1j*epsilon_v[n+N]*tau)
-
-
-	U_npn2=np.dot(rot_mat,np.dot(U_mm,np.transpose(rot_mat)))
-
-#	print(U_mm)
-
-
-
-	E_new3=0.
-	for m in range(-N,0):
-		for m2 in range(-N,N+1):
-			E_new3+=np.absolute(U_npn2[m2+N,m+N])**2*E_v[m2+N]
-	E_new3=E_new3/N
-
-
-	print(E_old1,E_new1,E_new3)	#H_1,H_0,H_1
-
-
-###########Energies at the fourth interval H0 H1 H0 H1
-#All in basis n of H0
-
+#############
 # H0
-	H_00=np.zeros((2*N+1,2*N+1))
+	H_00=np.zeros((2*N+1,2*N+1))			#in original basis nd diagonal
 	for m in range(-N,N+1):
 		H_00[m+N,m+N]=epsilon_vm[m+N]
+#	H_00=np.copy(Ham)
 
-	H_01=np.dot(rot_mat,np.dot(H_00,np.transpose(rot_mat)))
+#	Ham_diag=np.dot(rot_mat,np.dot(Ham,np.transpose(rot_mat)))
+
+	H_01=np.dot(rot_mat,np.dot(H_00,np.transpose(rot_mat)))	#in  basis m
 
 # H1
-	H_11=np.zeros((2*N+1,2*N+1))
+	H_11=np.zeros((2*N+1,2*N+1))		#in basis m diagonal
 	for m in range(-N,N+1):
-		H_11[m+N,m+N]=E_v[m+N]
-	H_10=np.dot(np.transpose(rot_mat),np.dot(H_11,rot_mat))
+		H_11[m+N,m+N]=E_true[m+N]
 
+	assert(np.all(abs(H_11-Ham_diag)<1e-8))
+
+	H_10=np.dot(np.transpose(rot_mat),np.dot(H_11,rot_mat))	#in original basis diagonal, so this is theoriginal Ham
+	assert(np.all(abs(Ham-H_10)<1e-8))
+
+
+#	plt.matshow(H_11-Ham_diag)
+#	plt.colorbar()
+#	plt.matshow(H_11)
+#	plt.colorbar()
+#	plt.matshow(Ham_diag)
+#	plt.colorbar()
+#	plt.show()
+
+
+
+#	plt.matshow(H_10)
+#	plt.colorbar()
+#	plt.show()
+
+
+#	print(H_10)
+
+##########################Time evolution
 #time evolution in H0
 	U_00=np.zeros((2*N+1,2*N+1),dtype=complex)
 	for m in range(-N,N+1):
-		U_00[m+N,m+N]=np.exp(-1j*epsilon_vm[m+N]*tau)
+		U_00[m+N,m+N]=np.exp(-1j*H_00[m+N,m+N]*tau)
 	U_01=np.dot(rot_mat,np.dot(U_00,np.transpose(rot_mat)))
+###	Ham_diag=np.dot(rot_mat,np.dot(Ham,np.transpose(rot_mat)))
 
-	
+
 
 #time evolution in H1
 	U_11=np.zeros((2*N+1,2*N+1),dtype=complex)
 	for m in range(-N,N+1):
-		U_11[m+N,m+N]=np.exp(-1j*E_v[m+N]*tau)
+		U_11[m+N,m+N]=np.exp(-1j*H_11[m+N,m+N]*tau)
 	U_10=np.dot(np.transpose(rot_mat),np.dot(U_11,rot_mat))
 
-#	U_20=np.dot(U_10,U_00)
 
-	n_step=50
+#Time evolution starting from eigenstates of H0
+#H0 H1 H0 H1 H0 ...
+
 	E_step=np.zeros((n_step))
 	U_rot=np.eye(2*N+1)
 	step=0
 
 	while (step < n_step):
 		A=np.dot(U_rot.conj().T,np.dot(H_00,U_rot))
-		for n in range(-N,0):
-			E_step[step]+=A[n+N,n+N]
+		for n in range(-N,N+1):
+			E_step[step]+=A[n+N,n+N].real*occup(epsilon_vm[n+N],mu,T)
 		step+=1
 		U_rot=np.dot(U_00,U_rot)
 		A=np.dot(U_rot.conj().T,np.dot(H_10,U_rot))
-		for n in range(-N,0):
-			E_step[step]+=A[n+N,n+N]
+		for n in range(-N,N+1):
+			E_step[step]+=A[n+N,n+N].real*occup(epsilon_vm[n+N],mu,T)
 		step+=1
 		U_rot=np.dot(U_10,U_rot)
 
@@ -301,8 +292,37 @@ def main():
 	print(E_step)
 
 
-	plt.plot(E_step,'b',label='g')
-#	plt.legend()
+#Time evolution starting from eigenstates of H1
+#nothing H1 H0 H1 H0 ...
+#	n_step=n_step
+	E_step1=np.zeros((n_step))
+	U_rot=np.eye(2*N+1)
+	step=0
+	
+	while (step < n_step):
+		A=np.dot(U_rot.conj().T,np.dot(H_11,U_rot))
+		for n in range(-N,0):
+			E_step1[step]+=A[n+N,n+N].real*occup(E_true[n+N],mu,T)
+		step+=1
+		U_rot=np.dot(U_11,U_rot)
+		A=np.dot(U_rot.conj().T,np.dot(H_01,U_rot))
+		for n in range(-N,0):
+			E_step1[step]+=A[n+N,n+N].real*occup(E_true[n+N],mu,T)
+		step+=1
+		U_rot=np.dot(U_01,U_rot)
+
+	E_step1=E_step1/N
+	print(E_step1)
+
+
+#	x=np.linspace(-1,1,1001)
+#	y=occup_v(x,0,0.01)
+#	plt.plot(x,y)
+
+	plt.plot(E_step[1:],'b',label='0')	#start from 1 so that they are in the same cycle, starting with H1
+	plt.plot(E_step1,'r',label='1')
+#	plt.plot(E_step[1:]-E_step[1:],'b',label='0')	#start from 1 so that they are in the same cycle, starting with H1
+	plt.legend()
 	plt.show()
 
 
